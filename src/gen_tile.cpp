fc9ef1e26f21644975cb224e2fe9bbaafa4d981f
Pierre Louvart <pierre.louvart@gmail.com>
2025-06-03 23:07:10 +0200

    - fix bounds

--- src/gen_tile.cpp
+++ src/gen_tile.cpp
@@ -126,14 +126,14 @@ struct projectionconfig *get_projection(const char *srs)
 		prj = (struct projectionconfig *)malloc(sizeof(struct projectionconfig));
 		prj->bound_x0 = 0.0;
 		prj->bound_y0 = 0.0;
-		prj->bound_x1 = 18421881.03421;
-		prj->bound_y1 = 36843762.0684;
+		prj->bound_x1 = 36885035.64084;
+		prj->bound_y1 = 36885035.64084;
 		// prj->bound_x0 = -18421881.03421;
 		// prj->bound_y0 = -18421881.03421;
 		// prj->bound_x1 = 18421881.03421;
 		// prj->bound_y1 = 18421881.03421; 
 		prj->aspect_x = 1;
-		prj->aspect_y = 2;
+		prj->aspect_y = 1;
 	} else if (strcmp(srs, "+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs") == 0) {
 		g_logger(G_LOG_LEVEL_DEBUG, "Using plate carree projection settings");
 		prj = (struct projectionconfig *)malloc(sizeof(struct projectionconfig));
